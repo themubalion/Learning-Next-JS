be4fb827fe2476acc488c0c41fa07bde88aa608f
Muhammad Mubashshir Ali <jssmithsa6516@gmail.com>
2022-07-03 21:13:39 +0530

    fixed the deploy error in master branch

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -29,15 +29,18 @@ function Blog() {
           <h2 className='my-2'>Our Latest Posts</h2>
 
           {Blog.map((blogitem)=>{
-            return (<Link href={'./blogpost/'+blogitem.slug}>
-            <div className="blogItem cursor-pointer my-6" key={blogitem.slug}>
+            return (<div key={blogitem.slug}>
+            <Link href={'./blogpost/'+blogitem.slug}>
+            <div className="blogItem cursor-pointer my-6">
               <div>
                 <h3 className=''>{blogitem.title}</h3>
                 <div>{blogitem.previewContent}</div>
               </div>
               <button className='bg-black text-white my-2 py-1 px-2 rounded-md'>Read More</button>
             </div>
-          </Link>)
+          </Link>
+          </div>
+          )
           }
           )}
 
